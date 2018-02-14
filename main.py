2a918ce9105beb07e729df892e56b529913d0110
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-02-14 19:33:23 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from sensorbase import DistanceSensor
+import time
+import sys
+
+sensors = [
+    '98:4f:ee:10:6c:78',
+    '98:4f:ee:0f:cc:60'
+]
+
+sensorObjs = []
+for i in sensors:
+    sensorObjs.append(DistanceSensor(i))
+
+[ i.start() for i in sensorObjs]
+
+try:
+    while True:
+        sys.stdout.write('---\r\n')
+        for i in sensorObjs:
+            sys.stdout.write(" sensor {0} : {1}\r\n".format(i.addr, i.state))
+            time.sleep(1)
+
+except SystemExit:
+    sys.stdout("detect")
+    pass
+
+except KeyboardInterrupt:
+    [ i.join() for i in sensorObjs]
+    sys.exit(0)
