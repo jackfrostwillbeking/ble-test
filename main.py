9c1e5fb81e6dab743f1471781cb8c18c687e4e40
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-03-08 19:40:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-from sensorbase import DistanceSensor
-import time
-import sys
-import os
 import subprocess
-
-BLE_DEVICE1 = os.getenv('BLE_DEVICE1', 'Cannot load the env')
-
-sensors = [
-    BLE_DEVICE1,
-    '98:4f:ee:0f:cc:60'
-]
-
-sensorObjs = []
-for i in sensors:
-    sensorObjs.append(DistanceSensor(i))
-
-[ i.start() for i in sensorObjs]
 
 res = subprocess.call(["/bin/bash","-c","./test_script.sh"])
 sys.stdout.write("########")
 sys.stdout.write("res")
 sys.stdout.write("########")
 
-try:
-    while True:
-        sys.stdout.write('---\r\n')
-        for i in sensorObjs:
-            sys.stdout.write(" sensor {0} : {1}\r\n".format(i.addr, i.state))
-            time.sleep(1)
-
-except SystemExit:
-    sys.stdout("detect")
-    pass
-
-except KeyboardInterrupt:
-    [ i.join() for i in sensorObjs]
-    sys.exit(0)
+sys.exit(0)
