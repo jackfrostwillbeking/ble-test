bd02fa6396b193343a11cde6870123b09afaf0e2
jackfrostwillbeking <jackfrostwillbeking@gmail.com>
2018-03-08 15:46:43 +0900

    update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -12,7 +12,7 @@ GET_TOKEN_RESULT=$(curl -X POST https://accounts.google.com/o/oauth2/token \
 -d "client_secret=H4bQe1RaElK6WXUfirfHSdEF" \
 -d "redirect_uri=urn:ietf:wg:oauth:2.0:oob" \
 -d "grant_type=authorization_code")
-function upload_file () {
+function upload_file() {
     curl -X POST https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart \
     -H "Authorization: Bearer $ACCESS_TOKEN" \
     -H "Content-Type: multipart/related" \
@@ -20,7 +20,7 @@ function upload_file () {
     -F "file=@$FILE;type=text/plain"
 }
 
-function refresh_access_token (){
+function refresh_access_token(){
     ACCESS_TOKEN=$(curl -X POST https://accounts.google.com/o/oauth2/token \
     -d "refresh_token=$REFRESH_TOKEN" \
     -d "client_id=$CLIENT_ID" \
